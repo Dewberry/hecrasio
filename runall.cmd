594fed12604e3f543128132b127beb311ec3cc89
autodewberry <Dewberry8401>
2019-08-28 20:17:34 +0000

    update point file paths

--- runall.cmd
+++ runall.cmd
@@ -1,5 +1,5 @@
-start python C:\Users\Administrator\Desktop\hecrasio\PostProcessor.py DC_F01_NBR_E0006 1 > p1.out
-start python C:\Users\Administrator\Desktop\hecrasio\PostProcessor.py DC_F01_NBR_E0006 2 > p2.out
-start python C:\Users\Administrator\Desktop\hecrasio\PostProcessor.py DC_F01_NBR_E0005 3 > p3.out
-start python C:\Users\Administrator\Desktop\hecrasio\PostProcessor.py DC_F01_NBR_E0005 4 > p4.out
-start python C:\Users\Administrator\Desktop\hecrasio\PostProcessor.py DC_F01_NBR_E0005 5 > p5.out
+start python C:\Users\Administrator\Desktop\hecrasio\PostProcessor.py DC_F02_NBR_E0006 P1 
+start python C:\Users\Administrator\Desktop\hecrasio\PostProcessor.py DC_F02_NBR_E0029 P2 
+start python C:\Users\Administrator\Desktop\hecrasio\PostProcessor.py DC_F02_NBR_E0035 P3 
+start python C:\Users\Administrator\Desktop\hecrasio\PostProcessor.py DC_F02_NBR_E0048 P4 
+start python C:\Users\Administrator\Desktop\hecrasio\PostProcessor.py DC_F02_NBR_E0057 P5 
