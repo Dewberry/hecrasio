8699a86b37792a35967a52a455b01432b49dd900
autodewberry <Dewberry8401>
2019-09-16 13:56:21 +0000

    new runs

--- manual_runs.cmd
+++ manual_runs.cmd
@@ -1,9 +1,17 @@
-start python C:\Users\Administrator\Desktop\scheduler_manual.py P1 simulations s3://pfra/Passaic/P05/H06/E0001/Passaic_P05_H06_E0001_in.zip
+start python C:\Users\Administrator\Desktop\scheduler_manual.py P1 simulations s3://pfra/Sacramento/P03/H12/E1001/Sacramento_P03_H12_E1001_in.zip
 timeout 
 
-start python C:\Users\Administrator\Desktop\scheduler_manual.py P2 simulations s3://pfra/Passaic/P05/H06/E0101/Passaic_P05_H06_E0101_in.zip
+start python C:\Users\Administrator\Desktop\scheduler_manual.py P2 simulations s3://pfra/Sacramento/P03/H24/E2101/Sacramento_P03_H24_E2101_in.zip
 timeout 
 
-start python C:\Users\Administrator\Desktop\scheduler_manual.py P3 simulations s3://pfra/Passaic/P05/H12/E1001/Passaic_P05_H12_E1001_in.zip
+start python C:\Users\Administrator\Desktop\scheduler_manual.py P3 simulations s3://pfra/Sacramento/P03/H24/E2001/Sacramento_P03_H24_E2001_in.zip
 timeout 
 
+start python C:\Users\Administrator\Desktop\scheduler_manual.py P4 simulations s3://pfra/Sacramento/P03/H96/E3101/Sacramento_P03_H96_E3101_in.zip
+timeout 
+
+start python C:\Users\Administrator\Desktop\scheduler_manual.py P5 simulations s3://pfra/Sacramento/P03/H96/E3001/Sacramento_P03_H96_E3001_in.zip
+timeout 
+
+start python C:\Users\Administrator\Desktop\scheduler_manual.py P6 simulations s3://pfra/Sacramento/P03/H06/E0001/Sacramento_P03_H06_E0001_in.zip
+timeout 
