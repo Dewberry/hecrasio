54de780b3f5d2d56adbd9d9acbd6aebe0f9391a6
bakinam <btaessawy@gmail.com>
2019-09-30 09:58:33 -0400

    post-processing the hec-ras sims

--- runall.cmd
+++ runall.cmd
@@ -4,8 +4,7 @@ start python C:\Users\Administrator\Desktop\pfra-dbase\scheduler.py P2 simulatio
 timeout 3
 start python C:\Users\Administrator\Desktop\pfra-dbase\scheduler.py P3 simulations
 timeout 3
-start python C:\Users\Administrator\Desktop\pfra-dbase\scheduler.py P4 simulations
-timeout 3
 start python C:\Users\Administrator\Desktop\pfra-dbase\scheduler.py P5 simulations
 timeout 3
 start python C:\Users\Administrator\Desktop\pfra-dbase\scheduler.py P6 simulations
+timeout 3
